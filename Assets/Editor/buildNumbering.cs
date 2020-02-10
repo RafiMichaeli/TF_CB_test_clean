06b733df9422842657169fe315275e36ee663338
rafimichaeli <rafi.michaeli@gmail.com>
2020-02-10 16:54:51 +0200

    added build numbering script
    build numberisnscript

---
+++ Assets/Editor/buildNumbering.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+private static void IncrementBuildNumber(int 1)
+    {
+        // Load the PlayerSettings asset.
+        var playerSettings = Resources.FindObjectsOfTypeAll<PlayerSettings>().FirstOrDefault();
+ 
+        if (playerSettings != null)
+        {
+            SerializedObject so = new SerializedObject(playerSettings);
+ 
+            // Find the build number property.
+            var sp = so.FindProperty("AndroidBundleVersionCode");
+ 
+            var currentValue = sp.longValue;
+ 
+            sp.longValue = currentValue + amount;
+ 
+            // Save player settings.
+            so.ApplyModifiedProperties();
+            AssetDatabase.SaveAssets();
+        }
+    }
